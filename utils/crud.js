466babf420f617dd0db1b2708b85203c83aab417
Joakim Romland <romland@gmail.com>
2023-04-17 13:44:25 +0200

    CRUD for levelData in level editor GPT-4: Had trouble getting a result in context-length; hence GPT4
    The CSS for the CRUD looks like crap, it will be fixed. There are still
    bugs as well, which I anticipate need human intervention. Sadly. Prompts
    below.
    
    >>> Prompt 1 (GPT-4):
    Using plain javascript (no dependencies) and HTML:
    
    Create a CRUD for e.g. data structure like below:
    
    levelData = {
        level : 1,
        name : "El-el-em-ing",
        seed : null, //1681139505452,
        gradients : [
            {
            type: 'linear',
            x0: 0,
            y0: 0,
            x1: 0,
            y1: 600,
            stops: [
                { offset: 0, color: 'black' },
                { offset: 1, color: '#660000' }
            ]
            }
        ],
        decorations : [
            {
            type: "organics",
            location: ["top"],
            }
        ],
        shapes : [],
        objects : [],
        start : { x : 25, y : -20 },
        finish : { x : 750, y : 500 },
    }
    
    - The CRUD system should be as generic as possible, so e.g. I pass in levelData to a function
      and it will look at e.g. the "name" field and determine that is a string and output a form field like
      <input type="text" id="levelData.name" value="El-el-em-ing"/>. Then do similar form fields
      appropriate for all datatypes. E.g. integer, coordinate, array, child-objects (the structure
      needs to be processed recursively), etc.
    - Add "+" and "-" actions on every array to add/remove items from the array.
    - The fields should be created dynamically. The above structure is just an example of the types
      of data the CRUD system should support.
    - take into account that some field values might be null or undefined
    - every field named 'type' should be a select field as there will be more types. The options for
      each select field will be passed in as a separate argument, e.g.: { gradients.type : [ "option 1", "option 2"] }
    - if a value is of type boolean, make it a checkbox
    - if a field is named something with color, make it a color dialog, make sure you check
      actual names of keys and not some index in an array
    - add a placeholder for 'save' that just prints the content of the modified structure
    - visually indent nested child-objects so you can see what they are children of
    - when a form field is changed or a new item is added, the original passed in object should be modified
    - in order to keep track of datatypes and structure you will want to need to keep a copy of the
      original structure around in case the last element of some part of the structure is deleted. So,
      check for datatypes/structure in a copy of what was passed in, but any modification should be to the original
    - No need to give me levelData object, I'll paste that in myself
    - Make the code brief, short variable names. E.g. short variable names like field as 'fld', options as 'opts',
      value as 'val', button as 'btn', document as 'doc', label as 'lbl', remove as 'rm', etc
    
    Give me only code, no explanation.
    
    >>> Prompt 2 (also GPT-4):
    Give me a minimal but decent looking CSS for a form with fields. Label and field on same row

---
+++ utils/crud.js
@@ -0,0 +1,129 @@
+"use strict";
+/**
+* >>> Prompt: editor/instructions/crud.0001.txt
+*/
+var CRUD = (function ()
+{
+    function inpCreator(id, val, type, lvl, hdlr)
+    {
+        const inp = document.createElement('input');
+        inp.id = id;
+        inp.value = val;
+        inp.style.marginLeft = lvl * 20 + "px";
+        if(type) {
+            inp.type = type;
+        }
+        if(hdlr) {
+            inp.addEventListener('input', (e) => hdlr(e));
+        }
+        return inp;
+    }
+
+    function dive(obj, path, opts, lvl = 0)
+    {
+        for (let key in obj) {
+            const lbl = document.createElement('label');
+            lbl.textContent = key;
+            lbl.style.marginLeft = `${20 * lvl}px`;
+            path.append(lbl);
+            const id = path.id + '.' + key;
+            
+            if (key === 'type' && opts) {
+                const sel = document.createElement('select');
+                opts["type"].forEach((o) => {
+                    const opt = document.createElement('option');
+                    opt.value = o;
+                    opt.textContent = o;
+                    sel.appendChild(opt);
+                });
+                sel.id = id;
+                sel.style.marginLeft = lvl * 20 + "px";
+                sel.value = obj[key];
+                sel.addEventListener('change', (e) => {
+                    obj[key] = e.target.value;
+                });
+                path.appendChild(sel);
+            } else if (key.toLowerCase().includes('color')) {
+                const colorInp = inpCreator(id, obj[key], 'color', lvl, (e) => {
+                    obj[key] = e.target.value;
+                });
+                path.appendChild(colorInp);
+            } else if (typeof obj[key] === 'string') {
+                const strInp = inpCreator(id, obj[key], 'text', lvl, (e) => {
+                    obj[key] = e.target.value;
+                });
+                path.appendChild(strInp);
+            } else if (typeof obj[key] === 'number') {
+                const numInp = inpCreator(id, obj[key], 'number', lvl, (e) => {
+                    obj[key] = parseFloat(e.target.value);
+                });
+                path.appendChild(numInp);
+            } else if (typeof obj[key] === 'boolean') {
+                const boolCb = document.createElement('input');
+                boolCb.type = 'checkbox';
+                boolCb.id = id;
+                boolCb.checked = obj[key];
+                boolCb.addEventListener('change', (e) => {
+                    obj[key] = e.target.checked;
+                });
+                path.appendChild(boolCb);
+            } else if (Array.isArray(obj[key])) {
+                const arrContainer = document.createElement('div');
+                arrContainer.id = id;
+                path.appendChild(arrContainer);
+                const updateArr = (container) => {
+                    container.innerHTML = '';
+                    obj[key].forEach((item, idx) => {
+                        const itemPath = document.createElement('div');
+                        itemPath.id = container.id + `[${idx}]`;
+                        container.appendChild(itemPath);
+                        dive(item, itemPath, opts?.[key], lvl + 1);
+                    });
+                    
+                    const addButton = document.createElement('button');
+                    addButton.textContent = '+ ' + key;
+                    
+                    addButton.addEventListener('click', () => {
+                        obj[key].push(JSON.parse(JSON.stringify(obj[key][0])));
+                        updateArr(container);
+                    });
+                    
+                    container.appendChild(addButton);
+                };
+                
+                updateArr(arrContainer);
+                
+                if (obj[key].length > 0) {
+                    const removeButton = document.createElement('button');
+                    removeButton.textContent = '- ' + key;
+                    removeButton.addEventListener('click', () => {
+                        obj[key].pop();
+                        updateArr(arrContainer);
+                    });
+                    path.appendChild(removeButton);
+                }
+            } else if (typeof obj[key] === 'object') {
+                const newObjPath = document.createElement('div');
+                newObjPath.id = id;
+                path.appendChild(newObjPath);
+                dive(obj[key], newObjPath, opts, lvl + 1);
+            }
+        }
+    }
+    
+    function create(data, options, elt, saveCB)
+    {
+        const root = document.createElement('div');
+        dive(data, root, options);
+        elt.appendChild(root);
+        
+        const btnSave = document.createElement('button');
+        btnSave.textContent = 'Save';
+        btnSave.addEventListener('click', (e) => saveCB(e, data));
+        elt.appendChild(btnSave);
+    }
+    
+    return {
+        create : create
+    }
+})();
