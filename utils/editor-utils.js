7b3bbebd93b450962e8690adc7c65e7722e329bd
Joakim Romland <romland@gmail.com>
2023-04-19 17:19:42 +0200

    Reduce hand-drawn points in editor ChatGPT: Unsurprisingly, it returned Ramer–Douglas–Peucker algorithm.
    >>> Prompt:
    Give me an algorithm and javascript code to simplify/reduce drawn coordinates
    like the one below to use less points:
    
    "points": [ { "x": 581, "y": 514 }, { "x": 584, "y": 512 }, { "x": 586, "y": 510 },
    { "x": 587, "y": 509 }, { "x": 590, "y": 508 }, { "x": 591, "y": 507 }, { "x": 592, "y": 506 },
    { "x": 595, "y": 506 }, { "x": 596, "y": 505 }, { "x": 598, "y": 505 }, { "x": 599, "y": 505 },
    { "x": 601, "y": 504 }, { "x": 602, "y": 504 }, { "x": 603, "y": 504 }, { "x": 605, "y": 504 }
    
    >>> Human note: There is really no need for the _really_ high resolution of hand-drawn points. This will reduce the length of those arrays drastically and making hand-drawing a more viable alternative when editing levels.

---
+++ utils/editor-utils.js
@@ -0,0 +1,52 @@
+var EditorUtils = (function () {
+    /*
+    >>> Prompt: editor/instructions/point-reduction.0001.txt
+
+    To simplify/reduce the number of points in drawn coordinates, we can use a technique
+    called Douglas-Peucker algorithm. This algorithm removes points that don’t contribute
+    to the overall shape of the line or polygon.
+    */
+    // Function that implements the Douglas Peucker algorithm to reduce points
+    function douglasPeucker(points, tolerance)
+    {
+        // Find the point with the maximum distance
+        let dmax = 0;
+        let index = 0;
+        const end = points.length - 1;
+        for (let i = 1; i < end; i++) {
+            let d = perpendicularDistance(points[i], points[0], points[end]);
+            if (d > dmax) {
+                index = i;
+                dmax = d;
+            }
+        }
+
+        // If max distance is greater than tolerance, recursively simplify
+        if (dmax > tolerance) {
+            // Recursive call
+            const left = points.slice(0, index + 1);
+            const right = points.slice(index);
+            const resultsLeft = douglasPeucker(left, tolerance);
+            const resultsRight = douglasPeucker(right, tolerance);
+
+            // Build the result list
+            return resultsLeft.slice(0, resultsLeft.length - 1).concat(resultsRight);
+        } else {
+            return [points[0], points[end]];
+        }
+    }
+
+    // Function that calculates the perpendicular distance from a point to a line
+    function perpendicularDistance(point, start, end) {
+        const { x: startX, y: startY } = start;
+        const { x: endX, y: endY } = end;
+        const { x: pointX, y: pointY } = point;
+        const numerator = Math.abs((endY - startY) * pointX - (endX - startX) * pointY + endX * startY - endY * startX);
+        const denominator = Math.sqrt(Math.pow((endY - startY), 2) + Math.pow((endX - startX), 2));
+        return numerator / denominator;
+    }
+
+    return {
+        douglasPeucker : douglasPeucker
+    }
+})();
